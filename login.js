8396c5be1609511f6015adb3b216c293443ffbd6
tiagofernandess <tiagofernandes0095@gmail.com>
2024-08-24 19:50:49 -0300

    tela de login pronta

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
-function enter() {
-    window.location.href = "https://tiagofernandess.github.io/Emprestimo-F-cil/home.html";
+function logIn() {
+    const userName = document.querySelector("#username").value;
+    const passWord = document.querySelector("#password").value;
+
+
+    if (userName === "tiago" && passWord === "123") {
+        window.location.href = "home.html";
+    } else {
+        document.querySelector("#error-message").textContent = "Usuário ou Senha incorreta!";
+    }
 }
+
